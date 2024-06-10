fbdf69a6fe13721d49cfced5a307385034179464
suchi <suchiths6528@gmail.com>
2024-06-10 14:31:50 +0530

    UPDATED

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function ProductsData(){
     const Products=await axios.get(
-        "http://fakestoreapiserver.vercel.app/amazonproducts"
+        "https://fakestoreapiserver.vercel.app/amazonproducts"
     );
     return Products;
 }
